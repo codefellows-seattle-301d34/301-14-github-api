373d735004d29cac92bf6626f426edf87555e711
mnfmnfm <mnfmnfm@gmail.com>
2018-05-24 11:45:26 -0700

    nice fixes

--- public/app.js
+++ public/app.js
@@ -10,4 +10,4 @@ $.ajax({
   .then(
     data => data.forEach(repo =>
       $('#results').append(`<h3>${repo.name}</h3><p>${repo.description}</p><hr>`)),
-    err => console.error(err.status, err.statusText, 'is the way my stuff is broken'));
+    err => console.error(`${err.status} ${err.statusText} is the way my stuff is broken.`));
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('./public'));
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+  res.sendFile('index.html');
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
